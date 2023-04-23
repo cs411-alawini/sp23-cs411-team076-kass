9dbd9fe81d161ad1a41656b2cdcfcf893bf22b9d
Ajit-Ravichandran <ajitravicchandran@gmail.com>
2023-04-23 17:37:48 -0500

    SPC

--- olympicstop/frontend/olympicstop/src/components/Medal/StoredProceduredFilter.js
+++ olympicstop/frontend/olympicstop/src/components/Medal/StoredProceduredFilter.js
@@ -1,63 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const StoredProceduredFilter = () => {
-  const [sportsParticipants, setSportsParticipants] = useState([]);
-  const [filteredParticipants, setFilteredParticipants] = useState([]);
-  const [error, setError] = useState(null);
-  const [selectedSport, setSelectedSport] = useState('');
-  const [selectedCountry, setSelectedCountry] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [sportsParticipants, setSportsParticipants] = useState([])
+  const [filteredParticipants, setFilteredParticipants] = useState([])
+  const [error, setError] = useState(null)
+  const [selectedSport, setSelectedSport] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://35.209.21.140:8000/filtered_sports_participants');
-        setSportsParticipants(response.data.sports_participants);
-        setFilteredParticipants(response.data.sports_participants);
-        setTimeout(() => setLoading(false), 3000); 
-      } catch (err) {
-        setError(err);
-      }
+    const fetchFilteredParticipants = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get('http://35.209.21.140:8000/filtered_sports_participants', {
+                params: {
+                    sport: selectedSport,
+                    country: selectedCountry,
+                },
+            });
+            setFilteredParticipants(response.data.sports_participants);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
     };
-    fetchData();
-  }, []);
 
-  const sports = [...new Set(sportsParticipants.map((participant) => participant.sport))];
-  const countries = [...new Set(sportsParticipants.map((participant) => participant.country))];
+    fetchFilteredParticipants();
+}, [selectedSport, selectedCountry]);
+
+
+  const sports = [
+    ...new Set(sportsParticipants.map(participant => participant.sport))
+  ]
+  const countries = [
+    ...new Set(sportsParticipants.map(participant => participant.country))
+  ]
 
   useEffect(() => {
     const filterParticipants = () => {
-      let filtered = sportsParticipants.filter((participant) => {
-        let sportMatch = !selectedSport || participant.sport === selectedSport;
-        let countryMatch = !selectedCountry || participant.country === selectedCountry;
-        return sportMatch && countryMatch;
-      });
-      setFilteredParticipants(filtered);
-    };
+      let filtered = sportsParticipants.filter(participant => {
+        let sportMatch = !selectedSport || participant.sport === selectedSport
+        let countryMatch =
+          !selectedCountry || participant.country === selectedCountry
+        return sportMatch && countryMatch
+      })
+      setFilteredParticipants(filtered)
+    }
 
     if (!loading) {
-      filterParticipants();
+      filterParticipants()
     }
-  }, [selectedSport, selectedCountry, sportsParticipants, loading]);
+  }, [selectedSport, selectedCountry, sportsParticipants, loading])
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   return (
     <div>
       <h1>Sports Participants</h1>
       <div>
-        <label htmlFor="sport-filter">Sport:</label>
+        <label htmlFor='sport-filter'>Sport:</label>
         <select
-          id="sport-filter"
+          id='sport-filter'
           value={selectedSport}
-          onChange={(e) => {
-            setSelectedSport(e.target.value);
+          onChange={e => {
+            setSelectedSport(e.target.value)
           }}
         >
-          <option value="">All</option>
+          <option value=''>All</option>
           {sports.map((sport, index) => (
             <option key={index} value={sport}>
               {sport}
@@ -66,15 +78,15 @@ const StoredProceduredFilter = () => {
         </select>
       </div>
       <div>
-        <label htmlFor="country-filter">Country:</label>
+        <label htmlFor='country-filter'>Country:</label>
         <select
-          id="country-filter"
+          id='country-filter'
           value={selectedCountry}
-          onChange={(e) => {
-            setSelectedCountry(e.target.value);
+          onChange={e => {
+            setSelectedCountry(e.target.value)
           }}
         >
-          <option value="">All</option>
+          <option value=''>All</option>
           {countries.map((country, index) => (
             <option key={index} value={country}>
               {country}
@@ -105,7 +117,7 @@ const StoredProceduredFilter = () => {
         </table>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default StoredProceduredFilter;
+export default StoredProceduredFilter
