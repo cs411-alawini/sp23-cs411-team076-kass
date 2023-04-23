12ab39786d46e11cf2be6390255a78404b54bbd3
Ajit-Ravichandran <ajitravicchandran@gmail.com>
2023-04-23 15:57:34 -0500

    adding chart

--- olympicstop/frontend/olympicstop/src/components/Medal/MedalTable.js
+++ olympicstop/frontend/olympicstop/src/components/Medal/MedalTable.js
@@ -1,3 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import './Medals.css'
+
 function MedalTable() {
   const [ranking, setRanking] = useState([]);
   const [lineChartData, setLineChartData] = useState([]);
