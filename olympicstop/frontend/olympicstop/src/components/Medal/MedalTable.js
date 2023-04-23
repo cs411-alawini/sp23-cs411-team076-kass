ef360b88065b921683334e44dc33b564dd487904
Ajit-Ravichandran <ajitravicchandran@gmail.com>
2023-04-23 16:33:07 -0500

    adding chart

--- olympicstop/frontend/olympicstop/src/components/Medal/MedalTable.js
+++ olympicstop/frontend/olympicstop/src/components/Medal/MedalTable.js
@@ -1,25 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from 'recharts'
 import './Medals.css'
 
-function MedalTable() {
-  const [ranking, setRanking] = useState([]);
-  const [lineChartData, setLineChartData] = useState([]);
-  const navigate = useNavigate();
+function MedalTable () {
+  const [ranking, setRanking] = useState([])
+  const [lineChartData, setLineChartData] = useState([])
+  const navigate = useNavigate()
 
   useEffect(() => {
-    async function fetchData() {
-      const responseRanking = await fetch('http://35.209.21.140:8000/ranking');
-      const dataRanking = await responseRanking.json();
-      setRanking(dataRanking.ranking);
+    async function fetchData () {
+      const responseRanking = await fetch('http://35.209.21.140:8000/ranking')
+      const dataRanking = await responseRanking.json()
+      setRanking(dataRanking.ranking)
 
-      const responseLineChart = await fetch('http://35.209.21.140:8000/ranking1');
-      const dataLineChart = await responseLineChart.json();
-      setLineChartData(dataLineChart.ranking1);
+      const responseLineChart = await fetch(
+        'http://35.209.21.140:8000/ranking1'
+      )
+      const dataLineChart = await responseLineChart.json()
+      setLineChartData(dataLineChart.ranking1)
     }
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <div className='medal-container'>
@@ -56,18 +66,26 @@ function MedalTable() {
           height={300}
           data={lineChartData}
           margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="country" />
-          <YAxis />
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='country' />
+          <YAxis domain={[0, 250]} />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line
+            type='monotone'
+            dataKey='total'
+            stroke='#8884d8'
+            activeDot={{ r: 8 }}
+          />
         </LineChart>
       </div>
     </div>
-  );
+  )
 }
-export default MedalTable;
+export default MedalTable
