7178ba21b212a2f9568b4223c2185d7d13a94de8
kavya-kar <kkarth3@illinois.edu>
2023-04-26 18:58:54 -0500

    New iFrame

--- olympicstop/frontend/olympicstop/src/components/Medal/StoredProceduredFilter.js
+++ olympicstop/frontend/olympicstop/src/components/Medal/StoredProceduredFilter.js
@@ -124,6 +124,11 @@ const StoredProceduredFilter = () => {
           </tbody>
         </table>
       )}
+       <iframe
+        src="http://35.209.21.140:8050"
+        title="Dash App"
+        style={{ width: "100%", height: "600px", border: "none" }}>
+        </iframe>
     </div>
   )
 }
